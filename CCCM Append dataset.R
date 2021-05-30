d4ffc5ca544bf835f166e23a545359b28a56be71
BacotRaphael <raphael.bacot@gmail.com>
2021-05-30 20:30:37 +0300

    june21

--- CCCM Append dataset.R
+++ CCCM Append dataset.R
@@ -15,12 +15,17 @@ require(openxlsx)
 source("./R/moveme.R")
 
 ### Load kobo choices file to vlook up old site codes
+survey_v1 <- read.xlsx("data/CCCM_Site_Reporting_Kobo_tool_(V1)_12042021.xlsx", sheet = "survey")
+survey_v2 <- read.xlsx("data/CCCM_Site_Reporting_Kobo_tool_(V2)_12042021.xlsx", sheet = "survey")
+choices_v1 <- read.xlsx("data/CCCM_Site_Reporting_Kobo_tool_(V2)_12042021.xlsx", sheet = "choices")
+choices_v2 <- read.xlsx("data/CCCM_Site_Reporting_Kobo_tool_(V2)_12042021.xlsx", sheet = "choices")
 choices <- read.csv("./data/kobo/choices.csv", check.names = T) %>% select(-X)
-external_choices <- read.csv("./data/kobo/choices_external.csv", check.names = F)
+external_choicesv1 <- read.xlsx("data/CCCM_Site_Reporting_Kobo_tool_(V1)_12042021.xlsx", sheet = "external_choices") %>% filter(list_name == "sitename")
+external_choicesv2 <- read.xlsx("data/CCCM_Site_Reporting_Kobo_tool_(V2)_12042021.xlsx", sheet = "external_choices") %>% filter(list_name == "sitename")
 
-external_choices <- filter(external_choices, external_choices$list_name == "sitename")
-names(external_choices)[names(external_choices) == "name"] <- "a4_site_code"
-names(external_choices)[names(external_choices) == "label::english"] <- "a4_site_name"
+# external_choices <- filter(external_choices, external_choices$list_name == "sitename")
+# names(external_choices)[names(external_choices) == "name"] <- "a4_site_code"
+# names(external_choices)[names(external_choices) == "label::english"] <- "a4_site_name"
 
 ## Load site ID master list
 id_list <- read.xlsx("./data/CCCM IDP Sites_site names and IDs_May 2021.xlsx")
@@ -29,18 +34,17 @@ id_list <- read.xlsx("./data/CCCM IDP Sites_site names and IDs_May 2021.xlsx")
 #################################### INTERNAL ##############################################################################
 
 ### Produce Internal Updated dataset ###
+
+# I. Import all necessary files with renaming of similar column accross V1 and V2 when necessary
+
 #### Internal Master file
-master_all_int <- read.xlsx("./data/CCCM_Site Reporting List_March 2021_ALL internal_incl. new site IDs.xlsx")
+master_all_int <- read.xlsx("./data/CCCM_Site Reporting List_March 2021_ALL internal_incl. new site IDs.xlsx") %>%
+  dplyr::rename(b4_site_cccm_agency_name = b2_site_smc_agency_name)
 
 ### Load Last Cleaned files ###
 ## Internal
 last_internal_v1 <- read.xlsx("./output/internal/CCCM_SiteReporting_V1 Internal_2021-05-19.xlsx")
 last_internal_v2 <- read.xlsx("./output/internal/CCCM_SiteReporting_V2 Internal_2021-05-19.xlsx")
-
-# last_internal_v1 <- last_internal_v1 %>%
-#   setnames(old = c(""),
-#            new = c(""))
-
 
 ## If only TRUE it means that no new values have been cleaned
 unique(last_internal_v1$uuid %in% master_all_int$uuid)
@@ -48,45 +52,36 @@ unique(last_internal_v2$uuid %in% master_all_int$uuid)
 
 ## Take latest dataset and remove the duplicated entries using the master
 new_v1 <- anti_join(last_internal_v1, master_all_int, "uuid") %>%
-  dplyr::rename(B1_CCCM_Pillars_existing_on_s.site_administration = B1_CCCM_Pillars_existing_on_s.site_administration__exu,
-                B1_CCCM_Pillars_existing.cccm_agency = B1_CCCM_Pillars_existing_on_s.site_management__cccm_agency,
-                d1_most_common_reason_idps_left_place_of_origin.security_concerns_conflict_explosives_lack_of_security_forces_war = d1_most_common_reason_idps_left_place_of_origin.security_concerns_conflict_explosives_lack_of_security_forces)
+  dplyr::rename(
+    # B1_CCCM_Pillars_existing_on_s.site_administration = B1_CCCM_Pillars_existing_on_s.site_administration__exu,       # check with christine that the idea is to recode the binary into a text question
+    B1_CCCM_Pillars_existing_on_s.site_management_supervision = B1_CCCM_Pillars_existing_on_s.site_management__cccm_agency,
+    d1_most_common_reason_idps_left_place_of_origin.security_concerns_conflict_explosives_lack_of_security_forces_war = d1_most_common_reason_idps_left_place_of_origin.security_concerns_conflict_explosives_lack_of_security_forces)
 
 new_v2 <- anti_join(last_internal_v2, master_all_int, "uuid") %>%
-  dplyr::rename(B1_CCCM_Pillars_existing_on_s.site_administration = B1_CCCM_Pillars_existing_on_s.site_administration_SCMCHAIC,
-                B1_CCCM_Pillars_existing.cccm_agency = B1_CCCM_Pillars_existing_on_s.site_supervision__cccm_agency,
-                d1_most_common_reason_idps_left_place_of_origin.security_concerns_conflict_explosives_lack_of_security_forces_war = d1_most_common_reason_idps_left_place_of_origin.war)
-
-# Column harmonisation check
-order(colnames(new_v2)[grepl("f1", colnames(new_v2))]) %in% order(colnames(new_v1)[grepl("f1", colnames(new_v1))])
+  dplyr::rename(
+    # B1_CCCM_Pillars_existing_on_s.site_administration = B1_CCCM_Pillars_existing_on_s.site_administration_SCMCHAIC,   # check with christine that the idea is to recode the binary into a text question
+    B1_CCCM_Pillars_existing_on_s.site_management_supervision = B1_CCCM_Pillars_existing_on_s.site_supervision__cccm_agency,
+    d1_most_common_reason_idps_left_place_of_origin.security_concerns_conflict_explosives_lack_of_security_forces_war = d1_most_common_reason_idps_left_place_of_origin.war) 
 
 new_int <- plyr::rbind.fill(new_v1, new_v2) %>%
-  setNames(tolower(colnames(.))) %>%                                                                  # Set names to lower cases to merge with master dataset
-  setNames(gsub("b1_CCCM_Pillars_existing_on_s.", "b1_CCCM_Pillars_existing.", colnames(.)))          # put existing instead of existings for some for harmonising check with Christine if there is a reason or if it could creates issues
+  setNames(tolower(colnames(.)))  %>%                                              # Set names to lower cases to merge with master dataset
+  mutate(b1_CCCM_Pillars_existing_on_s.site_administration = ifelse(b1_cccm_pillars_existing_on_s.site_administration__exu == 1, "exu",
+                                                                    ifelse(b1_cccm_pillars_existing_on_s.site_administration_scmchaic == 1, "scmchaic",
+                                                                           NA)))
 
-## Harmonisation of columns
-# To delete => test for the d1 question on reason leaving place of origin
-# colnames(new_int)[grepl("d1", colnames(new_int))] 
-# colnames(master_all_int)[grepl("d1", colnames(master_all_int))]
-# test <- master_all_int$d1_most_common_reason_idps_left_place_of_origin %>% unique
-# lapply(test, function(x) grepl(x, colnames(new_int)[grepl("d1", colnames(new_int))])) # checking that paste entries match column names
-# test <- master_all_int %>%
-#   mutate(sum.na = rowSums(across(matches("d1_"), ~is.na(.)))) %>%
-#   select(matches("d1_|sum.na"))
+## II. Harmonisation of columns headers + recoding of binary/text column when necessary
+
+# col1 <- colnames(new_v1)[grepl("B1", colnames(new_v1))]
+# col2 <- colnames(new_v2)[grepl("B1", colnames(new_v2))]
+# new_v1$B1_CCCM_Pillars_existing_on_s %>% unique
+# new_v2$B1_CCCM_Pillars_existing_on_s %>% unique
+# master_all_int$b1_site_management %>% unique
 
 ## Append the unique new entries to the final Master ALL Internal (new IDs still need to be added manually)
 new_master_all_int <- plyr::rbind.fill(master_all_int, new_int)
 # write.xlsx(new_master_all_int, paste0("./output/internal/CCCM_SiteReporting_All Internal (with some ID)_",today,".xlsx"))
 
-# Recoding threats to the site columns and harmonising with past data 
-# colnames(new_v1)[grepl("f1", colnames(new_v1))]
-# colnames(new_v2)[grepl("f1", colnames(new_v2))]
-# colnames(master_all_int)[grepl("f1", colnames(master_all_int))]
-# colnames(new_master_all_int)[grepl("f1", colnames(new_master_all_int))]
-# new_master_all_int <- new_master_all_int %>%
-#   mutate(sum.na = rowSums(across(matches("f1_threats_to_the_site"), ~is.na(.)))) %>%
-#   select(matches("f1_threats_to_the_site|sum.na"))
-
+# A. Question threats to the site - Harmonisation of  with past data 
 # Recoding the text column when there is no text entry but binary columns are filled out. 
 col <- colnames(new_master_all_int)[grepl("f1_threats_to_the_site.", colnames(new_master_all_int))]
 for (c in col){
@@ -110,48 +105,34 @@ new_master_all_int <- new_master_all_int %>%
          f1_threats_to_the_site = gsub("^ | $", "", f1_threats_to_the_site),
          f1_threats_to_the_site = gsub("infectious_diseasess", "infectious_diseases", f1_threats_to_the_site)
          )
-
+# for check, erase later
 # new_master_all_int$f1_threats_to_the_site %>% unique
 # all.choices.data <- new_master_all_int$f1_threats_to_the_site %>% str_split(" ") %>% unlist %>% as.data.frame %>% setNames("response") %>% group_by(response) %>%
 #   summarise(n())
 # choices %>% filter(grepl("f1", list_name))
 
-# d1_most_common_reason_idps_left_place_of_origin - Recoding resons idps left place of origin 
-# In V2 there is no other field in the kobo tool? => non matching columns
-# In masterlist, there is only the text column => recode the binary columns
-# d1_most_common_reason_idps_left_place_of_origin: security_concerns_conflict_explosives_lack_of_security_forces (V1) & war (V2) (new header: d1_most_common_reason_idps_left_place_of_origin.security_concerns_conflict_explosives_lack_of_security_forces_war)
+# B. Question Most common reason to leave place of origin - Harmonisation of  with past data
+# d1_most_common_reason_idps_left_place_of_origin => security_concerns_conflict_explosives_lack_of_security_forces (V1) & war (V2) (new header: d1_most_common_reason_idps_left_place_of_origin.security_concerns_conflict_explosives_lack_of_security_forces_war)
+# Note 1: In V2 there is no other field in the kobo tool? => non matching columns! Warning, might introduce a bias in analysis later
+# Note 2: In masterlist, there is only the text column => recode the binary columns
 
-# 1. Recode the text response with the new header for the new category
-# test <- new_master_all_int %>%
-#   mutate(sum.na = rowSums(across(matches("d1"), ~is.na(.)))) %>%
-#   select(matches("d1|sum.na"))
+# B.1. Recode the text response with the new header for the new category
 new_master_all_int <- new_master_all_int %>%
   mutate(d1_most_common_reason_idps_left_place_of_origin = gsub("security_concerns_conflict_explosives_lack_of_security_forces|war", "security_concerns_conflict_explosives_lack_of_security_forces_war", d1_most_common_reason_idps_left_place_of_origin),
          f1_threats_to_the_site.conflict_related_incidents_war = f1_threats_to_the_site.conflict_related_incidents + f1_threats_to_the_site.war) %>%
   select(-f1_threats_to_the_site.conflict_related_incidents, -f1_threats_to_the_site.war)
 
-# 2. Appropriately recode the binary when there is a non na text response and the binary columns are NA.
-col <- colnames(test)[grepl("d1_most_common_reason_idps_left_place_of_origin.", colnames(test))]
+# B.2. Appropriately recode the binary when there is a non na text response and the binary columns are NA.
+col <- colnames(new_master_all_int)[grepl("d1_most_common_reason_idps_left_place_of_origin.", colnames(new_master_all_int))]
 for (c in col){
-  test <- test %>% 
+  new_master_all_int <- new_master_all_int %>% 
     mutate(!!sym(c) := ifelse(!is.na(!!sym(c)), !!sym(c), 
                               ifelse(is.na(!!sym(c)) & (!is.na(d1_most_common_reason_idps_left_place_of_origin)) & grepl(gsub("d1_most_common_reason_idps_left_place_of_origin.", "", c), d1_most_common_reason_idps_left_place_of_origin), 1, 
                                      ifelse(is.na(!!sym(c)) & (!is.na(d1_most_common_reason_idps_left_place_of_origin)) & !grepl(gsub("d1_most_common_reason_idps_left_place_of_origin.", "", c), d1_most_common_reason_idps_left_place_of_origin), 0, NA)))) 
   }
-# check <- test %>% filter(sum.na==10) 
 
-# Recoding select one text entries into the corresponding binary columns, keeping NA when relevant
-# For primary cooking space question, and reason for leaving place of origin
-
-# Check the number of NAs across columns 
-# test <- new_master_all_int %>%
-#   mutate(sum.na = rowSums(across(matches("d1_most_common_reason_idps_left_place_of_origin"), ~is.na(.))))
-# test2 <- test %>%
-#   select(matches("d1_most_common_reason_idps_left_place_of_origin|sum.na")) %>%
-#   filter(!d1_most_common_reason_idps_left_place_of_origin %in% "not_available")
-#   
-#   filter(sum.na == 10)    # If you look at safe_cooking_practices column, we don't have entry that are not coded so far.
-# test$d1_most_common_reason_idps_left_place_of_origin %>% unique
+# Recoding select one as select multiple, keeping NA when relevant
+# Question Reason for leaving place of origin + primary cooking space question
 
 new_master_all_int <- new_master_all_int %>%
   mutate_at(vars(matches("primary_cooking_space.")), function(x) ifelse(!is.na(x), x,
@@ -160,13 +141,13 @@ new_master_all_int <- new_master_all_int %>%
   mutate_at(vars(matches("d1_most_common_reason_idps_left_place_of_origin.")), function(x) ifelse(!is.na(x), x,
                                                                                                    ifelse(new_master_all_int$d1_most_common_reason_idps_left_place_of_origin %>% tolower == gsub("d1_most_common_reason_idps_left_place_of_origin.", "", deparse(substitute(x))), 1, 0)))
 
-# Merging marginalized people (V1) with minorities (V2) + recoding when text column is NA with valid entries in binary columns
+# C. Question vulnerable group - marginalized people (V1) with minorities (V2) + recoding when text column is NA with valid entries in binary columns
 new_master_all_int <- new_master_all_int %>%
   mutate(c7_presence_of_particularly_vulnerable_groups.marginalized_people_minorities =
            c7_presence_of_particularly_vulnerable_groups.marginalized_people + c7_presence_of_particularly_vulnerable_groups.minorities, .after = "c7_presence_of_particularly_vulnerable_groups") %>%
   select(-c7_presence_of_particularly_vulnerable_groups.marginalized_people, -c7_presence_of_particularly_vulnerable_groups.minorities)
 
-# Replacing past all past string entries wrongly coded with the correct label from the kobo tool:
+# C.1. Replacing past all past string entries wrongly coded with the correct label from the kobo tool:
 choices.vulnerable <- choices %>% filter(list_name=="qc06R") %>% select(-list_name, -label..arabic) # Extracting the label and names from question
 pattern <- choices.vulnerable$label..english                                    # Extract the labels in english to be replace
 replacement <- choices.vulnerable$name                                          # Extract the replacement correct name
@@ -183,17 +164,8 @@ new_master_all_int <- new_master_all_int %>%
          c7_presence_of_particularly_vulnerable_groups = gsub("child headed hh", "child_headed_hh", c7_presence_of_particularly_vulnerable_groups),
          c7_presence_of_particularly_vulnerable_groups = gsub("female headed hh", "female_headed_hh", c7_presence_of_particularly_vulnerable_groups),
          c7_presence_of_particularly_vulnerable_groups = gsub(" people ", " ", c7_presence_of_particularly_vulnerable_groups)) 
-# d <- new_master_all_int$c7_presence_of_particularly_vulnerable_groups %>% str_split(" ") %>% unlist %>% as.data.frame %>% setNames("response") %>%
-#   group_by(response) %>% summarise(n())
-# e <- unique(new_master_all_int$c7_presence_of_particularly_vulnerable_groups)
-# test <- new_master_all_int %>% filter(!grepl(replacement, c7_presence_of_particularly_vulnerable_groups)) %>%
-#   select(matches("c7_presence_of_particularly_vulnerable_groups"))
-# 
-# test <- new_master_all_int %>%
-#   mutate(sum.na = rowSums(across(matches("c7_presence_of_particularly_vulnerable_groups"), ~is.na(.)))) %>%
-#   select(matches("c7_presence_of_particularly_vulnerable_groups|sum.na")) %>% filter(sum.na==1)
 
-# Recoding the text column when there is no text entry but binary columns are filled out. 
+# C.2. Recoding the text column when there is no text entry but binary columns are filled out. 
 col <- colnames(new_master_all_int)[grepl("c7_presence_of_particularly_vulnerable_groups.", colnames(new_master_all_int))]
 for (c in col){
   new_master_all_int <- new_master_all_int %>%
@@ -201,19 +173,16 @@ for (c in col){
                                                                   paste(c7_presence_of_particularly_vulnerable_groups, gsub("c7_presence_of_particularly_vulnerable_groups.", "", deparse(substitute(!!sym(c)))), sep = " "), c7_presence_of_particularly_vulnerable_groups))
 }
 
-# Cleaning the NA entries that were copied from the original text column
+# C.3. Cleaning the NA entries that were copied from the original text column
 new_master_all_int <- new_master_all_int %>%
   mutate(c7_presence_of_particularly_vulnerable_groups = gsub("NA | NA | NA", "", c7_presence_of_particularly_vulnerable_groups))
-# final.check <- new_master_all_int %>% filter(sum.na == 1)
 
-# Relocate columns => To be done
+# Relocate columns => Check if necessary
 
-
-### Internal Site ID code
+### Internal Site ID code => will be moved in data cleaning script
 #new_master_all_int$a4_site_name <- str_trim(new_master_all_int$a4_site_name, "both")
 #new_master_all_int$a4_site_code <- external_choices$a4_site_code[match(new_master_all_int$a4_site_name, external_choices$a4_site_name)]
 #new_master_all_int <- new_master_all_int[order(new_master_all_int$a4_site_code, new_master_all_int$a4_site_name, na.last = FALSE),]
-
 #new_master_all_int <- new_master_all_int %>% mutate(a4_site_code = ifelse(is.na(a4_site_code) == TRUE, paste0(a2_district_code,"_",seq(16147, 5000)), paste0(a4_site_code)))
 #new_master_all_int$a4_site_code
 
